aa08b2c5a36c2d773ad7d5a892d44fb32dbc400d
Parshwa218 <66219453+Parshwa218@users.noreply.github.com>
2020-06-01 01:39:38 +0530

    Add files via upload

---
+++ PortScanner.py
@@ -0,0 +1,31 @@
+import socket 
+
+
+print("*********************************************************")
+print("*                                                       *")
+print("*                                                       *")
+print("*                                                       *")
+print("             Coded by Parshwa Bhavsar                    ")
+print("*                                                       *")
+print("*                                                       *")
+print("*                                                       *")
+print("*********************************************************")
+
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.settimeout(5)
+
+host = input("Enter the IP you want to scan: ")
+port = int(input("Enter the Port you want to scan: "))
+
+
+def portScanner(port):
+	if s.connect_ex((host,port)):
+		print("The port is closed")
+	else:
+		print("The port is open")
+
+portScanner(port)
+
+
+print("********************  Thank You ************************")
